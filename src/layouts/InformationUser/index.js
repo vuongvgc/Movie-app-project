43a355c5ae7763498582f98c7d9f1230a78f0c74
vuongvgc <vuongvgc@gmail.com>
2021-03-19 07:53:54 +0700

    InforUserForm: call API, Fill in form

--- src/layouts/InformationUser/index.js
+++ src/layouts/InformationUser/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getUser } from "../../actions/User";
+import InforUserForm from "../../components/InforUserForm";
+import _ from "lodash";
 class InformationUser extends Component {
-  render() {
+  componentDidMount() {
     this.props.getUser({ taikhoan: "vuongcybersort" });
-    return <div>InformationUser</div>;
+  }
+  render() {
+    console.log(this.props.userDetail);
+    if (!this.props.userDetail) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div className="container-fluid">
+        <InforUserForm
+          initialValues={_.pick(
+            this.props.userDetail,
+            "taiKhoan",
+            "matKhau",
+            "hoTen",
+            "email",
+            "soDT"
+          )}
+        />
+      </div>
+    );
   }
 }
-export default connect(null, { getUser })(InformationUser);
+const mapStateToProps = (state) => {
+  return {
+    userDetail: state.userReducers.userDetail,
+  };
+};
+export default connect(mapStateToProps, { getUser })(InformationUser);
