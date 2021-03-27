a8f0cbd98083fa78afcfbe2c416cd9c5b5d98a58
vuongvgc <vuongvgc@gmail.com>
2021-03-27 07:43:01 +0700

    AdminLayout:funtion add user: layout and get data

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,156 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-const Modal = (props) => {
-  const color = {
-    backgroundColor: "rgba(0,0,0,0.5)",
+import RegisterForm from "../RegisterForm";
+import { connect } from "react-redux";
+import { register } from "../../actions/Auth";
+class Modal extends React.Component {
+  onSubmit = (formValue) => {
+    console.log(formValue);
   };
-  return ReactDOM.createPortal(
-    <div class="modal fade" id="myModal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <header class="head-form mb-0 text-center m-2">
-            <h3 id="header-title">Cập Nhật Thông Tin</h3>
-          </header>
-          <div class="modal-body">
-            <form role="form" id="formNhanVien">
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="msnv"
-                    name="msnv"
-                    id="msnv"
-                    class="form-control input-sm"
-                    placeholder="Mã số nhân viên"
-                  />
-                </div>
-
-                <span class="sp-thongbao" id="tbMaNV"></span>
-              </div>
-
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-address-book"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="name"
-                    name="name"
-                    id="name"
-                    class="form-control input-sm"
-                    placeholder="Họ và tên"
-                  />
-                </div>
-                <span class="sp-thongbao" id="tbTen"></span>
-              </div>
-
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-envelope"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    class="form-control input-sm"
-                    placeholder="Email"
-                  />
-                </div>
-
-                <span class="sp-thongbao" id="tbEmail"></span>
-              </div>
-
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-key"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    class="form-control input-sm"
-                    placeholder="Mật khẩu"
-                  />
-                </div>
-                <span class="sp-thongbao" id="tbMatKhau"></span>
-              </div>
-
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-calendar"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    name="ngaysinh"
-                    id="datepicker"
-                    class="form-control"
-                    placeholder="Ngày sinh"
-                  />
-                </div>
-
-                <span class="sp-thongbao" id="tbNgay"></span>
-              </div>
-
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-briefcase"></i>
-                    </span>
-                  </div>
-                  <select class="form-control" id="chucvu">
-                    <option>Chọn chức vụ</option>
-                    <option>Sếp</option>
-                    <option>Trưởng phòng</option>
-                    <option>Nhân viên</option>
-                  </select>
-                </div>
-
-                <span class="sp-thongbao" id="tbChucVu"></span>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer" id="modal-footer">
-            <button id="btnThemNV" type="button" class="btn btn-success">
-              Thêm người dùng
-            </button>
-            <button
-              id="btnCapNhat"
-              type="button"
-              class="btn btn-success"
-              data-dismiss="modal"
-            >
-              Cập nhật
-            </button>
-            <button
-              id="btnDong"
-              type="button"
-              class="btn btn-danger"
-              data-dismiss="modal"
-            >
-              Đóng
-            </button>
+  render() {
+    const color = {
+      backgroundColor: "rgba(0,0,0,0.5)",
+    };
+    return ReactDOM.createPortal(
+      <div className="modal fade" id="myModal">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <header className="head-form mb-0 text-center m-2">
+              <h3 id="header-title">Cập Nhật Thông Tin</h3>
+            </header>
+            <div className="modal-body">
+              <RegisterForm admin={true} onSubmit={this.onSubmit} />
+            </div>
           </div>
         </div>
-      </div>
-    </div>,
-    document.querySelector("#modal")
-  );
-};
-export default Modal;
+      </div>,
+      document.querySelector("#modal")
+    );
+  }
+}
+export default connect(null, { register })(Modal);
