3ba4a26c84b9271e8113d6ff5200b293097e55c3
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2021-04-05 23:09:36 +0700

    reponsive moviedetail fix accesstoken

--- src/actions/Booking.js
+++ src/actions/Booking.js
@@ -1,10 +1,13 @@
 import { BOOKING_FAIL, BOOKING_REQUEST, BOOKING_SUCCESS } from "../constants/Booking";
 import axios from "../utils/axiosClient";
 
+
+let token = "";
 const user = localStorage.getItem('user')
-
-
-const { accessToken } = JSON.parse(user)
+if(user){
+    const { accessToken } = JSON.parse(user)
+    token = accessToken;
+}
 
 export const getBooking = (value) => {
     return (dispatch) => {
@@ -12,7 +15,7 @@ export const getBooking = (value) => {
         axios.post(
             `QuanLyDatVe/DatVe`, value, {
             headers: {
-                Authorization:`Bearer ${accessToken}`
+                Authorization:`Bearer ${token}`
             }
         }
         )
