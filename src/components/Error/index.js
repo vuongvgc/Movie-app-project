de3898d74ded5638b5523abfa5c4ef49a5acec76
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2021-04-08 23:36:11 +0700

    sua lai trang ticketroom, sua loi css bi lan sang may component khac, tat console.log o trang em lam.

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -7,9 +7,9 @@ export default function index() {
     return (
         <div className="error">
             <h1 className="error__logo">4<span><i class="fas fa-ghost"></i></span>4</h1>
-            <h2>Error: 404 page not found</h2>
-            <p>Sorry, the page you're looking for cannot be accessed</p>
-            <button className="btn btn-success btn__error">
+            <h2 className="error__title">Error: 404 page not found</h2>
+            <p className="error__subTitle">Sorry, the page you're looking for cannot be accessed</p>
+            <button className="btn btn-success btn__error error__nav">
                 <NavLink to="/"> TRỞ VỀ TRANG CHỦ </NavLink>
             </button>
         </div>
